71001ee0cd93b78ac8922edb3c09a4983a2aad74
Max Robinson & Madeleine <pair+Maxwell-Robinson.madeleine-rashbrooke@devbootcamp.com>
2016-07-25 16:42:38 +1200

    Now have simple template working on server

--- views/template.js
+++ views/template.js
@@ -1,10 +1,12 @@
 var handlebars = require('handlebars')
 
-var data = {
-  para: "Hi, i'm colour paragraph"
-}
-var simpleTemplate = '<p>{{para}}</p>'
-var createSimpleResult = handlebars.compile(simpleTemplate)
-var simpleResult = createSimpleResult(data)
 
-console.log(simpleResult)
+var template = function (data) {
+  var templateText = '<p>{{para}}</p>'
+  var smartTemplate = handlebars.compile(templateText)
+  var html = smartTemplate(data)
+  return html
+}
+
+
+module.exports = template
