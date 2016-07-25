58155153af1858ba6bd15d44ddb821978d9c406e
Max Robinson & Madeleine <pair+Maxwell-Robinson.madeleine-rashbrooke@devbootcamp.com>
2016-07-25 16:44:21 +1200

    Now have simple template working on server

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-var handlebars = require('handlebars')
+var express = require('express')
+var template = require('./views/template')
+var app = express()
 
-var data = {
-  para: "Hi, i'm colour paragraph"
-}
-var simpleTemplate = '<p>{{para}}</p>'
-var createSimpleResult = handlebars.compile(simpleTemplate)
-var simpleResult = createSimpleResult(data)
 
-console.log(simpleResult)
+
+app.get('/', function (req, res) {
+  var ourText = '<div>Hi there</div>'
+  res.send(ourText)
+})
+
+app.get('/test', function (req, res) {
+  var ourData = {
+    para: "Hi, i'm colour paragraph"
+  }
+
+  var ourText = template(ourData)
+  res.send(ourText)
+})
+
+app.listen(3000)
+console.log("server is up and listening")
